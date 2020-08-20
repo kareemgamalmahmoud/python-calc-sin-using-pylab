a1262dc83cda812686ec35850cb3d5f5b61ebe75
kareemgamalmahmoud <51359449+kareemgamalmahmoud@users.noreply.github.com>
2020-08-20 20:38:13 +0200

    Add files via upload

---
+++ sinFun.py
@@ -0,0 +1,18 @@
+import math
+import pylab
+
+xmin , xmax = -2. * math.pi, 2. * math.pi
+
+n = 1000
+x = [0.] * n
+y = [0.] * n
+
+dx = (xmax - xmin)/(n-1)
+
+for i in range(n):
+    xpt = xmin + i * dx
+    x[i] = xpt
+    y[i] = math.sin(xpt) ** 2
+    
+pylab.plot( x , y )
+pylab.show()    
